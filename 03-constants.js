22658479b1614e6bb3c0c3e8d122606ac6d678b6
Weber Mori <webermori@gmail.com>
2018-01-13 18:40:04 -0200

    Constantes

---
+++ 03-constants.js
@@ -0,0 +1,7 @@
+var num = 2;
+//num = 4;
+var obj = {
+    name: 'Weber Mori'
+};
+obj.name = 'Teste';
+console.log(obj);
---
+++ 03-constants.ts
@@ -0,0 +1,9 @@
+const num = 2;
+//num = 4;
+
+const obj = {
+    name: 'Weber Mori'
+};
+
+obj.name = 'Teste';
+
